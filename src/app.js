a7cd2e0f2476f52774eb610947d40c6fc39deae9
ByeolNabi <20183338@office.deu.ac.kr>
2024-05-01 22:23:54 +0900

    refactor[express]: 최상위 path를 "/api"로 변경.
    Express 애플리케이션의 모든 URL 최상위 path를 "/api"로 변경했습니다.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
-dotenv.config();
+const mongoose = require("mongoose");
 
-app.listen(process.env.PORT);
+const app = express();
+const apiRouter = express.Router(); // 경로 사용을 위한 라우터 사용
+dotenv.config();
 
 const indexRouter = require("./routes/indexRoute");
 
-app.use("", indexRouter);
+app.use("/api", apiRouter); // 최상위 path를 "/api"로 지정하기
+apiRouter.use("", indexRouter);
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`server port is ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
