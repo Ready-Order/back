29b706f044e5a38ea06ee17d4afdbbdadb68d779
ByeolNabi <20183338@office.deu.ac.kr>
2024-05-02 19:23:02 +0900

    feat[express]: /menus에서 RUD에 해당하는 핸들러 개발.
    /menus 엔드포인트를 위해 읽기(Read), 업데이트(Update), 삭제(Delete) 기능을 구현하는 핸들러 개발. 이제 클라이언트가 메뉴 아이템을 조회, 수정, 삭제할 수 있음.

--- src/controller/menuController.js
+++ src/controller/menuController.js
@@ -16,27 +16,86 @@ const createMenuItem = async (req, res) => {
   } catch (err) {
     console.log(err);
     console.log("😇");
-    return res.json(err);
+    return res.status(500).json(err);
   }
 
   res.status(StatusCodes.CREATED).json({ menuItem: createdMenuItem });
 };
 
-const getAllMenuItems = (req, res) => {
-  res.json({ message: "this work" });
+const getAllMenuItems = async (req, res) => {
+  let menuItems;
+  try {
+    menuItems = await MenuItem.find();
+  } catch (err) {
+    console.log(err);
+    console.log("😇");
+    return res.status(500).json(err);
+  }
+  res.status(StatusCodes.OK).json(menuItems);
 };
 
-const updateMenuItem = (req, res) => {
-  res.json({ message: "this work" });
+const getMenuItem = async (req, res) => {
+  const { menuItemId } = req.params;
+
+  let menuItem;
+  try {
+    menuItem = await MenuItem.findById(menuItemId);
+  } catch (err) {
+    console.log(err);
+    console.log("😇");
+    return res.status(500).json(err);
+  }
+  return res.json({ menuItem });
 };
 
-const deleteMenuItem = (req, res) => {
-  res.json({ message: "this work" });
+const updateMenuItem = async (req, res) => {
+  const { menuItemId } = req.params;
+  let { title, image_url, price } = req.body;
+  price = parseInt(price);
+
+  let menuItem;
+  try {
+    menuItem = await MenuItem.findById(menuItemId);
+  } catch (err) {
+    console.log(err);
+    console.log("😇");
+    return res.status(500).json(err);
+  }
+
+  menuItem.title = title;
+  menuItem.image_url = image_url;
+  menuItem.price = price;
+
+  try {
+    await menuItem.save();
+  } catch (err) {
+    console.log(err);
+    console.log("😇");
+    return res.status(500).json(err);
+  }
+
+  res.status(200).json({ menuItem: menuItem.toObject({ getters: true }) });
+};
+
+const deleteMenuItem = async (req, res) => {
+  const { menuItemId } = req.params;
+
+  let menuItem;
+  try {
+    menuItem = await MenuItem.findByIdAndDelete(menuItemId);
+  } catch (err) {
+    console.log(err);
+    console.log("😇");
+    return res.status(500).json(err);
+  }
+
+  res.status(200).json({ message: "Deleted place.", menuItemId: menuItem._id });
 };
 
 module.exports = {
   createMenuItem,
   getAllMenuItems,
+  getMenuItem,
   updateMenuItem,
   deleteMenuItem,
 };
