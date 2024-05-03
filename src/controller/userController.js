0132a3a581ed0e2360b157915287f8b7943e8b1a
ByeolNabi <20183338@office.deu.ac.kr>
2024-05-03 14:31:12 +0900

    feat[express]: users 엔드포인트의 join 기능 구현.
    users 엔드포인트에서 사용자 가입 기능을 구현. MongoDB에 사용자의 정보를 정상적으로 저장할 수 있음. 추후에 비밀번호 암호화 기능을 추가할 예정

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,12 +1,41 @@
 const StatusCodes = require("http-status-codes");
 
-const user = require("../models/user");
+const User = require("../models/user");
 
 const login = (req, res) => {
   res.json({ message: "this work" });
 };
-const join = (req, res) => {
-  res.json({ message: "this work" });
+const join = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    console.log(err);
+    console.log("😇");
+    return res.status(500).json({ message: "server error" });
+  }
+
+  if (existingUser) {
+    return res.status(422).json({ message: "user already exist" });
+  }
+
+  const createdUser = new User({
+    name,
+    email,
+    password,
+  });
+
+  try {
+    await createdUser.save();
+  } catch (err) {
+    console.log(err);
+    console.log("😇");
+    return res.status(500).json(err);
+  }
+
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
 module.exports = { login, join };
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { login, join } = require("../controller/userController");
 
 /*  "/users"  */
-router.post("/login", login); // create
-router.get("/join", join); // read
+router.get("/login", login); // create
+router.post("/join", join); // read
 
 module.exports = router;
