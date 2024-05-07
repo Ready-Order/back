395dc58352c8495640d56308d675cb56e8fa458c
ByeolNabi <20183338@office.deu.ac.kr>
2024-05-07 16:23:21 +0900

    refactor[express]: menu 스키마 업데이트 및 API 코드 수정.
    menu 스키마를 업데이트하고, 변경된 스키마에 맞춰 API 코드를 수정.

--- src/models/menuItem.js
+++ src/models/menuItem.js
@@ -6,6 +6,7 @@ const menuSchema = new Schema({
   title: { type: String, required: true },
   image_url: { type: String, required: true },
   price: { type: Number, required: true },
+  tags: [{ type: String, required: false }],
   creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
 });
 
